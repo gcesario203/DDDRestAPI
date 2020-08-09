86b0580c15d2e1d9f0ed088e7e5d9d6eece39ebc
Gabriel Cesario <cesario203@outlook.com>
2020-08-09 20:53:32 -0300

    Finalizando camada de aplicação

--- RestAPIDDDModel/ApplicationServiceCliente.cs
+++ RestAPIDDDModel/ApplicationServiceCliente.cs
@@ -2,9 +2,7 @@
 using DDDApplication.Interface;
 using DDDApplication.Interface.Mappers;
 using DDDCore.Interfaces.Services;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DDDApplication
 {
@@ -13,29 +11,40 @@ namespace DDDApplication
         private readonly IServiceCliente serviceCliente;
         private readonly IMapperCliente mapperCliente;
 
+        public ApplicationServiceCliente(IServiceCliente serviceCliente, IMapperCliente mapperCliente)
+        {
+            this.serviceCliente = serviceCliente;
+            this.mapperCliente = mapperCliente;
+        }
+
         public void Add(ClienteDto clienteDto)
         {
-            throw new NotImplementedException();
+            var cliente = mapperCliente.MapperDtoToEntity(clienteDto);
+            serviceCliente.Add(cliente); 
         }
 
         public IEnumerable<ClienteDto> GetAll()
         {
-            throw new NotImplementedException();
+            var clientes = serviceCliente.GetAll();
+            return mapperCliente.MapperListClienteDto(clientes);
         }
 
         public ClienteDto GetById(int id)
         {
-            throw new NotImplementedException();
+            var cliente = serviceCliente.GetById(id);
+            return mapperCliente.MapperEntityToDto(cliente);
         }
 
         public void Remove(ClienteDto clienteDto)
         {
-            throw new NotImplementedException();
+            var cliente = mapperCliente.MapperDtoToEntity(clienteDto);
+            serviceCliente.Remove(cliente);
         }
 
         public void Update(ClienteDto clienteDto)
         {
-            throw new NotImplementedException();
+            var cliente = mapperCliente.MapperDtoToEntity(clienteDto);
+            serviceCliente.Update(cliente);
         }
     }
 }
---
+++ RestAPIDDDModel/ApplicationServiceProduto.cs
@@ -0,0 +1,51 @@
+﻿using DDDApplication.Dtos;
+using DDDApplication.Interface;
+using DDDApplication.Interface.Mappers;
+using DDDCore.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDApplication
+{
+    class ApplicationServiceProduto : IApplicationServiceProduto
+    {
+        private readonly IServiceProduto serviceProduto;
+        private readonly IMapperProduto mapperProduto;
+
+        public ApplicationServiceProduto(IServiceProduto serviceProduto, IMapperProduto mapperProduto)
+        {
+            this.serviceProduto = serviceProduto;
+            this.mapperProduto = mapperProduto;
+        }
+        public void Add(ProdutoDto produtoDto)
+        {
+            var produto = mapperProduto.MapperDtoToEntity(produtoDto);
+            serviceProduto.Add(produto);
+        }
+
+        public IEnumerable<ProdutoDto> GetAll()
+        {
+            var produtos = serviceProduto.GetAll();
+            return mapperProduto.MapperListProdutosDto(produtos);
+        }
+
+        public ProdutoDto GetById(int id)
+        {
+            var produto = serviceProduto.GetById(id);
+            return mapperProduto.MapperEntityToDto(produto);
+        }
+
+        public void Remove(ProdutoDto produtoDto)
+        {
+            var produto = mapperProduto.MapperDtoToEntity(produtoDto);
+            serviceProduto.Remove(produto);
+        }
+
+        public void Update(ProdutoDto produtoDto)
+        {
+            var produto = mapperProduto.MapperDtoToEntity(produtoDto);
+            serviceProduto.Update(produto);
+        }
+    }
+}
